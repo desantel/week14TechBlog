8e647d1eb0b3a82bc660cbdc6fddeea836c5e3c6
desantel <ecdesantis21@gmail.com>
2021-07-06 16:22:40 -0400

    added homeRoutes and seed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,35 +1,76 @@
-// const router = require('express').Router();
-// const { User, Blog } = require('../models');
-// const withAuth = require('../utils/auth');
+const router = require('express').Router();
+const { User, Blog } = require('../models');
+const withAuth = require('../utils/auth');
 
-// //stops users not logged in from accessing pages
-// router.get('/', withAuth, async (req, res) => {
-//     try {
-//         const userData = await User.findAll({
-//             attributes: { exclude: ['password'] },
-//             order: [['name', 'ASC']],
-//         });
-//         const users = userData.map((project) => project.get({ plain:true }));
+//stops users not logged in from accessing pages
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
-//         res.render('homepage', {
-//             users,
-//             logged_in: req.session.logged_in,
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-// router.get('/login', (req,res) => {
-//     if (req.session.logged_in) {
-//         res.redirect('/');
-//         return;
-//     }
-// });
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// //new comment
-// router.post('/new', withAuth, async (req,res) => {
-//     res.render('user', {logged_in: true})
-// })
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
-// module.exports = router;
+        const blog = blogData.get({ plain: true });
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('login')
+});
+
+module.exports = router;

f6dfd485a61aea792a4e14e0b8c3eca9339b2e0a
desantel <ecdesantis21@gmail.com>
2021-07-15 19:55:32 -0400

    views

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,10 +2,9 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
     const description = document.querySelector('#project-desc').value.trim();
   
-    if (name && needed_funding && description) {
+    if (name &&  description) {
       const response = await fetch(`/api/blogs`, {
         method: 'POST',
         body: JSON.stringify({ name, description }),
